0665fc226cff89f3166ab482b08b2a3ad43ad867
Oyedele Olufemi <itz4mesays@gmail.com>
2023-07-05 23:31:51 +0100

    added watcher

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 new Vue({
     el: '#app',
     data: {
-        from: 'NGN',
+        from: 'GBP',
         to: 'USD',
         amount: null,
         currencies: {},
-        result: null
+        result: 0,
+        loading: false
     },
     methods: {
         convertCurrency() {
-            const amount = Number(this.amount)
+            // const amount = Number(this.amount)
+            // this.loading = true
+            this.result = null;
 
-            axios.get(`https://currency-converter5.p.rapidapi.com/currency/convert?format=json&from=${this.from}&to=${this.to}&amount=${this.amount}`, {
-                headers: {
-                    'X-RapidAPI-Key': '69936f8ac1mshe283958e7128754p1f0a3bjsnc8e1194f3435',
-                    'X-RapidAPI-Host': 'currency-converter5.p.rapidapi.com'
-                }
-            })
-            .then(response => {
-                console.log(response)
-            })
+            // axios.get(`https://currency-converter5.p.rapidapi.com/currency/convert?format=json&from=${this.from}&to=${this.to}&amount=${this.amount}`, {
+            //     headers: {
+            //         'X-RapidAPI-Key': '69936f8ac1mshe283958e7128754p1f0a3bjsnc8e1194f3435',
+            //         'X-RapidAPI-Host': 'currency-converter5.p.rapidapi.com'
+            //     }
+            // })
+            // .then(response => {
+            //     console.log(response)
+            // })
+            // this.loading = false
+            this.result = 10
         },
         getCurrencies(){
             const currencies = localStorage.getItem('currencies')
@@ -39,7 +44,23 @@ new Vue({
         }
     },
     computed: {
-
+        calculateResult(){
+            return Number(this.amount) * this.result
+        },
+        disabled(){
+            return this.amount === 0 || !this.amount || this.loading
+        }
+    },
+    watch: {
+        from(){
+            this.result = 0
+        },
+        to(){
+            this.result = 0
+        },
+        amount(){
+            this.result = 0
+        }
     },
     mounted() {
 
